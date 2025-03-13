5a8aecd8963232b44fd8f5c1217b1b4fc2a7e2c7
Isaac <jxie4@nd.edu>
2025-03-13 15:09:13 +0800

    created Springboot backend

---
+++ La-Album-backend/src/main/java/com/g10/LaAlbumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.g10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaAlbumBackendApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(LaAlbumBackendApplication.class, args);
+    }
+}
